a7d61b1d80c067f47d8607d68b8e5e0119b1ebdc
Fernando <fbuono@fi.uba.ar>
2022-10-27 00:45:53 -0300

    funciones CrearHeap, CrearHeapArr y HeapSort finalizadas, faltan los tests

--- cola_prioridad_test.go
+++ cola_prioridad_test.go
@@ -2,11 +2,12 @@ package cola_prioridad_test
 
 import (
 	TDAHeap "cola_prioridad"
+	//"fmt"
 	"testing"
 )
 
 func TestHeap(t *testing.T) {
-	heap := TDAHeap.CrearHeap(func(a, b int) int { return a - b })
+	/**heap := TDAHeap.CrearHeap(func(a, b int) int { return a - b })
 	heap.Encolar(2)
 	heap.Encolar(9)
 	heap.Encolar(5)
@@ -15,4 +16,11 @@ func TestHeap(t *testing.T) {
 	heap.Encolar(10)
 	heap.Desencolar()
 	heap.Desencolar()
+	arr := []int{4, 2, 3, 2, 8, 6, 7, 8, 10}
+	heapArr := TDAHeap.CrearHeapArr(arr, func(a, b int) int { return a - b })
+	fmt.Println(heapArr.VerMax())
+	**/
+	arr2 := []int{4, 2, 3, 2, 8, 6, 7, 8, 11, 2222}
+
+	TDAHeap.HeapSort(arr2, func(a, b int) int { return a - b })
 }
--- heap.go
+++ heap.go
@@ -4,39 +4,59 @@ import (
 	"fmt"
 )
 
+const _CAPACIDAD_INICIAL = 10
+const _VECES_A_AUMENTAR = 2
+const _VECES_A_REDUCIR = 2
+const _VALOR_PARA_REDUCIR = 4
+
 type heap[T comparable] struct {
-	datos    [10]T
+	datos    []T
 	cantidad int
 	cmp      func(T, T) int
 }
 
 func CrearHeap[T comparable](funcion_cmp func(T, T) int) ColaPrioridad[T] {
 	heap := new(heap[T])
-	heap.datos = [10]T{}
+	heap.datos = make([]T, _CAPACIDAD_INICIAL)
 	heap.cmp = funcion_cmp
 	return heap
 }
+
+func CrearHeapArr[T comparable](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	heap := new(heap[T])
+	heap.datos = arreglo
+	heap.cmp = funcion_cmp
+	heap.cantidad = len(arreglo)
+	for i := len(heap.datos) - 1; i >= 0; i-- {
+		downheap(heap.datos, i, heap.cmp)
+	}
+	return heap
+}
+
+func HeapSort[T comparable](elementos []T, funcion_cmp func(T, T) int) {
+	for i := len(elementos) - 1; i >= 0; i-- {
+		downheap(elementos, i, funcion_cmp)
+	}
+	fmt.Println(elementos)
+	for i := 0; i < len(elementos); i++ {
+		elementos[0], elementos[len(elementos)-1-i] = elementos[len(elementos)-1-i], elementos[0]
+		downheap(elementos[:len(elementos)-1-i], 0, funcion_cmp)
+	}
+}
+
+// Métodos de ColaPrioridad
 
 func (h *heap[T]) EstaVacia() bool {
 	return h.cantidad == 0
 }
 
 func (h *heap[T]) Encolar(elemento T) {
+	if h.cantidad == cap(h.datos) {
+		h.redimensionar(cap(h.datos) * _VECES_A_AUMENTAR)
+	}
 	h.datos[h.cantidad] = elemento
 	h.cantidad++
-	h.upheap(&(h.datos), h.cantidad-1)
-	fmt.Println(h.datos)
-}
-
-func (h *heap[T]) upheap(datos *[10]T, pos_hijo int) {
-	if pos_hijo <= 0 {
-		return
-	}
-	pos_padre := (pos_hijo - 1) / 2
-	if h.cmp(datos[pos_padre], datos[pos_hijo]) > 0 {
-		datos[pos_padre], datos[pos_hijo] = datos[pos_hijo], datos[pos_padre]
-		h.upheap(datos, pos_padre)
-	}
+	upheap(h.datos, h.cantidad-1, h.cmp)
 }
 
 func (h *heap[T]) VerMax() T {
@@ -50,39 +70,64 @@ func (h *heap[T]) Desencolar() T {
 	if h.EstaVacia() {
 		panic("La cola está vacia")
 	}
+	if h.cantidad <= cap(h.datos)/_VALOR_PARA_REDUCIR && cap(h.datos) > _CAPACIDAD_INICIAL {
+		h.redimensionar(cap(h.datos) / _VECES_A_REDUCIR)
+	}
 	h.datos[0], h.datos[h.cantidad-1] = h.datos[h.cantidad-1], h.datos[0]
 	h.cantidad--
-	fmt.Println(h.datos)
-	h.downheap(&(h.datos), 0)
+	downheap(h.datos, 0, h.cmp)
 	return h.datos[h.cantidad]
-}
-
-func (h *heap[T]) downheap(datos *[10]T, pos_padre int) {
-	if pos_padre >= h.cantidad-1 {
-		return
-	}
-	pos_hijo_izq := 2*pos_padre + 1
-	pos_hijo_der := 2*pos_padre + 2
-	if pos_hijo_izq >= h.cantidad || pos_hijo_der >= h.cantidad {
-		return
-	}
-	pos_reemplazo := h.buscarReemplazo(datos, pos_padre, pos_hijo_izq, pos_hijo_der)
-	fmt.Println(pos_reemplazo)
-	if datos[pos_reemplazo] != datos[pos_padre] {
-		datos[pos_padre], datos[pos_reemplazo] = datos[pos_reemplazo], datos[pos_padre]
-		fmt.Println(h.datos)
-		h.downheap(datos, pos_reemplazo)
-	}
-
-}
-
-func (h *heap[T]) buscarReemplazo(datos *[10]T, pos_padre, pos_hijo_izq, pos_hijo_der int) int {
-	if h.cmp(datos[pos_hijo_izq], datos[pos_hijo_der]) < 0 {
-		return pos_hijo_izq
-	}
-	return pos_hijo_der
 }
 
 func (h *heap[T]) Cantidad() int {
 	return h.cantidad
+}
+
+// Métodos / funciones auxiliares
+
+func upheap[T comparable](datos []T, pos_hijo int, func_cmp func(T, T) int) {
+	if pos_hijo <= 0 {
+		return
+	}
+	pos_padre := (pos_hijo - 1) / 2
+	if func_cmp(datos[pos_padre], datos[pos_hijo]) < 0 {
+		datos[pos_padre], datos[pos_hijo] = datos[pos_hijo], datos[pos_padre]
+		upheap(datos, pos_padre, func_cmp)
+	}
+}
+
+func downheap[T comparable](datos []T, pos_padre int, func_cmp func(T, T) int) {
+	if pos_padre >= len(datos) {
+		return
+	}
+	pos_hijo_izq := 2*pos_padre + 1
+	pos_hijo_der := 2*pos_padre + 2
+	if pos_hijo_izq >= len(datos) && pos_hijo_der >= len(datos) {
+		return
+	}
+	var pos_reemplazo int
+	if pos_hijo_der >= len(datos) {
+		pos_reemplazo = pos_hijo_izq
+	} else {
+		pos_reemplazo = buscarReemplazo(datos, pos_padre, pos_hijo_izq, pos_hijo_der, func_cmp)
+	}
+	if datos[pos_reemplazo] != datos[pos_padre] {
+		datos[pos_padre], datos[pos_reemplazo] = datos[pos_reemplazo], datos[pos_padre]
+		downheap(datos, pos_reemplazo, func_cmp)
+	}
+}
+
+func buscarReemplazo[T comparable](datos []T, pos_padre, pos_hijo_izq, pos_hijo_der int, func_cmp func(T, T) int) int {
+	if func_cmp(datos[pos_hijo_der], datos[pos_padre]) > 0 && func_cmp(datos[pos_hijo_izq], datos[pos_hijo_der]) <= 0 {
+		return pos_hijo_der
+	} else if func_cmp(datos[pos_hijo_izq], datos[pos_padre]) > 0 && func_cmp(datos[pos_hijo_izq], datos[pos_hijo_der]) > 0 {
+		return pos_hijo_izq
+	}
+	return pos_padre
+}
+
+func (h *heap[T]) redimensionar(nuevaCapacidad int) {
+	nueva := make([]T, nuevaCapacidad)
+	copy(nueva, h.datos)
+	h.datos = nueva
 }
